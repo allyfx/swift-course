1e4b72bd0de701c81540bf958c9909824645f11a
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2023-04-18 09:38:01 -0300

    feat: session 2

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Alícia Foureaux on 14/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
